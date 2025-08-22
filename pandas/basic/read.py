1f6067da521609c100e7022e77855e6e77342d14
777-data-wizard <leopardbanana876@gmail.com>
2025-08-22 07:01:43 -0700

    read json

--- pandas/basic/read.py
+++ pandas/basic/read.py
@@ -2,4 +2,9 @@ import pandas as pd
 
 pd.options.display.max_rows = 9999999
 df = pd.read_csv('titanic.csv')
+print(df.head()) 
+print(df.loc[range(0, 7)])
 print(df)
+
+df1 = pd.read_json('test.json')
+print(df1.head(1))
